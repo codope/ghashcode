0a328f3975e1ea05536d1667c1973c6a5fe198b0
Sagar Sumit <sagar.sumit@ITIN000133-MAC.local>
2019-03-01 15:21:57 +0530

    Change the path to where input resides.

--- hashcode2019/PhotoSlideshowSolver.java
+++ hashcode2019/PhotoSlideshowSolver.java
@@ -5,7 +5,7 @@ public class PhotoSlideshowSolver {
     public static void main(String[] args) {
         String[] inputs = {"a_example", "b_lovely_landscapes", "c_memorable_moments", "d_pet_pictures", "e_shiny_selfies"};
         for (String input : inputs) {
-            SlideSimulator slideSimulator = new SlideSimulator("photos/" + input);
+            SlideSimulator slideSimulator = new SlideSimulator("data/photos/" + input);
             slideSimulator.parseFile();
         }
     }
